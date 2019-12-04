e1d96ca7c8515d255bce92c3c115137717447c86
Nicolas Thuylie <nicolas.thuylie@laposte.net>
2019-12-04 11:33:59 +0100

    Calculate satellite position

--- doppler.py
+++ doppler.py
@@ -1,35 +1,20 @@
 import numpy as np
-
+# ftp://cddis.gsfc.nasa.gov/pub/gps/products/wwww/igswwwwd.sp3.Z | template ephemeris
 
 class Doppler:
+    # Compute basic parameters at request time
+    def __init__(self, toe,t_data,mu0, delta_n, e, omega0, Cws, Cwc, a, Crc, Crs, Cic, Cis, omega_ascension0, omega_dot_ascension, i0, i_dot):
 
-    def __init__():
-        # Compute basic parameters at request time
-        toe = None
-        t_data = None
-        mu0 = None
-        delta_n = None
-        e = None
-        omega0 = None
-        Cws = None
-        Cwc = None
-        a= None
-        Crc = None
-        Crs = None
-        Cic = None
-        Cis = None
-        omega_ascension0 = None
-        omega_dot_ascension = None
-        i0 = None
-        i_dot = None
 
 
         GM = 3.986004418*np.power(10,14)
-        omega_e = 7.2921151467*np.power(10,-5)
+        omega_e = 7.2921151467*10**-5
 
         t = t_data-toe
         mu  = mu0 + (np.sqrt(GM/np.power(a,3)) + delta_n) * t
-        # E = mu + e * np.sin(E) developpement limité
+        # E = mu + e * np.sin(E)
+        E = np.roots([e/6,0,(1-e),-mu])
+        E = np.real(np.select(np.imag(E)==0,E))
         v = np.arctan((np.sqrt(1-e**2)*np.sin(E))/(np.cos(E)-e))
 
         # Correct for orbital perturbations
@@ -44,9 +29,15 @@ class Doppler:
         r_vect = np.array([r*np.cos(v),r*np.sin(v),0])
 
         # Rotation matrix
-        R = [[np.cos(omega_ascension)*np.cos(omega)-np.sin(omega_ascension)*np.sin(omega)*np.cos(i), -np.cos(omega_ascension)*np.sin(omega)-np.sin(omega_ascension)*np.cos(omega)*np.cos(i), np.sin(omega_ascension)*np.sin(i)]
+        R = np.asarray([[np.cos(omega_ascension)*np.cos(omega)-np.sin(omega_ascension)*np.sin(omega)*np.cos(i), -np.cos(omega_ascension)*np.sin(omega)-np.sin(omega_ascension)*np.cos(omega)*np.cos(i), np.sin(omega_ascension)*np.sin(i)]
         ,[np.sin(omega_ascension)*np.cos(omega)+np.cos(omega_ascension)*np.sin(omega)*np.cos(i), -np.sin(omega_ascension)*np.sin(omega)+np.cos(omega_ascension)*np.cos(omega)*np.cos(i), -np.cos(omega_ascension)*np.sin(i)]
-        ,[np.sin(omega)*np.sin(i), np.cos(omega)*np.sin(i), np.cos(i)]]
+        ,[np.sin(omega)*np.sin(i), np.cos(omega)*np.sin(i), np.cos(i)]])
+        
+
+        print(np.dot(R,r_vect))
+
+if __name__ == "__main__":
+    velocity = Doppler(208784,1574756786,1.28612756881,0.489020369661*10**-8,0.260362529662*10**-2,0.7931942133,0.622682263388*10**-5,0.1460313797*10**-5,0.515365547943*10**4,0.25971875*10**3,0.2853125*10**2,-0.204890966415*10**-7,0.577419996262*10**-7,0.108886242411*10,-0.815426822943*10**-8,0.963852456438,0.431089385164*10**-9)
 
 
 
