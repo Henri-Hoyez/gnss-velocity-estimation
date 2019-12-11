1b10e0508a863b5bb2143e44600760c28a99220d
Unknown <bapt.caen@gmail.com>
2019-12-11 14:14:02 +0100

    parser position

--- parserRinex.py
+++ parserRinex.py
@@ -32,10 +32,6 @@ def formatFichier(file):
         for k in range(len(ligneTab)):
             ligne2+=ligneTab[k]
         tabFinal.append(ligne2)
-        if(j%100==0):
-            print(tabFinal[j])
-            print(j)
-        j+=1
         ligne=f.readline() 
         f2.write(ligne2)
     f.close()
@@ -48,9 +44,10 @@ def obsToDataframe(file):
     df=pd.read_csv("data/"+file,delim_whitespace=True,header=None,skiprows=lambda x:x<=29,names=['name','pseudo_range','carrier_phase','doppler','signal_strength','4','5'])
     df['second_of_week']=df['pseudo_range']
     i=0
-    df['n_week']=get_gps_week()
     #index des lignes de temps
     indexRef=df[df['name']==">"].index
+    date1=date(int(df.loc[indexRef[0],'pseudo_range']),int(df.loc[indexRef[0],'carrier_phase']),int(df.loc[indexRef[0],'doppler']))
+    df['n_week']=get_gps_week(date1)
     val=get_second_of_week(datetime(int(df.loc[indexRef[0],'pseudo_range']),int(df.loc[indexRef[0],'carrier_phase']),int(df.loc[indexRef[0],'doppler']),int(df.loc[indexRef[0],'signal_strength']),int(df.loc[indexRef[0],'4']),int(df.loc[indexRef[0],'5'])))-1
     
 
@@ -70,13 +67,43 @@ def obsToDataframe(file):
 
 # Fonction finale à utiliser : 
 #       Paramètre : nom du fichier .obs (exemple avec "autoroute_apres_midi.obs")
-#
+
 def obsToDataframeFinal(file):
     goodFile=formatFichier(file)
     df=obsToDataframe(goodFile)
     return df
-# formatFichier("autoroute_apres_midi.obs")
-# df=obsToDataframe("tamponObs.txt")
+
+
+def posToDataframe(file):
+    df=pd.read_csv("data/"+file,delim_whitespace=True,header=13)
+    del df['Q'],df['ns'],df['sdx(m)'],df['sdy(m)'],df['sdz(m)'],df['sdyz(m)'],df['sdzx(m)'],df['age(s)'],df['ratio'],df['sdxy(m)']
+
+
+    dateStr=df.loc[0,'%']
+    dateTab=dateStr.split("/")
+    date1=date(int(dateTab[0]),int(dateTab[1]),int(dateTab[2]))
+    df['n_week']=get_gps_week(date1)
+
+
+    heureStr=df.loc[0,'GPST']
+    heureStr=heureStr.replace(".",":")
+    heureTab=heureStr.split(":")
+    sec0=get_second_of_week(datetime(int(dateTab[0]),int(dateTab[1]),int(dateTab[2]),int(heureTab[0]),int(heureTab[1]),int(heureTab[2])))
+    df['second_of_week']=sec0
+    
+    for i in range(len(df['second_of_week'])):
+        df.loc[i,'second_of_week']=sec0+i
+    del df['%'],df['GPST']
+
+
+    df=df.set_index(['n_week','second_of_week'])
+    df=df.rename(columns={"x-ecef(m)": "X" , "y-ecef(m)":"X" , "z-ecef(m)":"Z"})
+
+    return df
+
+
+# EXEMPLE :
+# df=obsToDataframeFinal("autoroute_apres_midi.obs")
 # print(df)
-df=obsToDataframeFinal("autoroute_apres_midi.obs")
+df=posToDataframe("autoroute_apres_midi.pos")
 print(df)
